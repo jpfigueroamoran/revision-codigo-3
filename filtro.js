29a4d0269939b07496d00d9d2d1b218a49bede9c
jp.figueroamoran <jp.figueroamoran@gmail.com>
2023-06-01 11:24:00 -0600

    Se revisó el código y se realizaron modificaciones

--- filtro.js
+++ filtro.js
@@ -1,63 +1,63 @@
-// Tenemos un li de productos
+document.addEventListener("DOMContentLoaded", function() {
+  // Tu código JavaScript aquí
+  const productos = [
+    {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
+    {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
+    {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
+    {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
+    {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  ];
 
-const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+  const ul = document.getElementById("lista-de-productos");
+  const $i = document.querySelector('.input');
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productos[i].nombre;
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
-
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
-}
-
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
-
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
-  }
-
-  const texto = $i.value;
-  console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
     var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
-}
+    imagen.setAttribute('src', productos[i].img);
 
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+    d.appendChild(ti);
+    d.appendChild(imagen);
+
+    ul.appendChild(d);
+  }
+
+  const botonDeFiltro = document.getElementById("boton-filtrar");
+
+  botonDeFiltro.onclick = function() {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+
+    const texto = $i.value;
+    console.log(texto);
+    const productosFiltrados = filtrado(productos, texto);
+
+    for (let i = 0; i < productosFiltrados.length; i++) {
+      var d = document.createElement("div");
+      d.classList.add("producto");
+
+      var ti = document.createElement("p");
+      ti.classList.add("titulo");
+      ti.textContent = productosFiltrados[i].nombre;
+
+      var imagen = document.createElement("img");
+      imagen.setAttribute('src', productosFiltrados[i].img);
+
+      d.appendChild(ti);
+      d.appendChild(imagen);
+
+      ul.appendChild(d);
+    }
+  };
+
+  const filtrado = (productos = [], texto) => {
+    return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+  };
+});
